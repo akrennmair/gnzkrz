acb959270e23530002e8b3d6a274bbe894af565d
Andreas Krennmair <ak@synflood.at>
2012-03-19 19:28:06 +0100

    switched UI to bootstrap.

--- gnzkrz.pl
+++ gnzkrz.pl
@@ -11,8 +11,15 @@ my $html_header = <<END;
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<link rel="stylesheet" href="{base}/_a/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{base}/_a/css/bootstrap.min.css">
+<style>
+body { padding-top: 60px; }
+</style>
 <title>{title}</title>
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 </head>
 <body>
 END
@@ -22,11 +29,45 @@ my $html_footer = <<END;
 </html>
 END
 
+sub get_menu($) {
+	my $active = shift;
+	my $home_active = ($active eq "home" ? ' class="active"' : "");
+	my $faq_active = ($active eq "faq" ? ' class="active"' : "");
+	my $imprint_active = ($active eq "imprint" ? ' class="active"' : "");
+	my $html_menu = <<END;
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<a class="brand" href="{base}/">krzz.de</a>
+			<div class="nav-collapse">
+				<ul class="nav">
+					<li$home_active><a href="{base}/"><i class="icon-home icon-white"></i>Home</a></li>
+					<li$faq_active><a href="{base}/_/faq"><i class="icon-book icon-white"></i>FAQ</a></li>
+					<li><a href="mailto:admin\@krzz.de"><i class="icon-envelope icon-white"></i>Contact</a></li>
+					<li><a href="https://github.com/akrennmair/gnzkrz"><i class="icon-file icon-white"></i>Show me the code!</a></li>
+					<li$imprint_active><a href="{base}/_/imprint"><i class="icon-info-sign icon-white"></i>Impressum</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+END
+	return $html_menu;
+}
+
 sub connect_db;
 sub encode_id($);
 sub decode_id($);
 sub error_unknown_url_id($);
 sub error_invalid_url($);
+sub slurp($);
 
 get '/' => sub {
 	my ($dbh, $prefix) = connect_db;
@@ -37,19 +78,20 @@ get '/' => sub {
 	$sth->finish;
 	$dbh->disconnect;
 
+	my $html_menu = get_menu("home");
+
 	t(<<END, base => base, url_count => $row->{url_count}, access_count => $row->{access_count}, title => "krzz.de | URL Shortener");
 $html_header
-<div id=main>
-<h1>Enter URL you want to shorten:</h1>
-<form action="{base}/_/save" method=post>
-<input id=url name=url placeholder="URL, e.g. http://example.com/" type=url required autofocus>
-<button type=submit id=submit>Shorten</button>
+$html_menu
+<div class="container">
+<h2>Shorten URL</h2>
+<form action="{base}/_/save" method="post" class="well">
+<label>Enter URL you want to shorten:
+<input id=url name=url type=url placeholder="URL, e.g. http://example.com/" required autofocus class="input-medium" style="width: 50%">
+</label>
+<button class="btn btn-primary" type=submit id=submit>Shorten</button>
 </form>
-</div>
-<div id=fineprint>
-<a href="{base}/_a/faq.html">FAQ</a> | <a href="mailto:admin\@krzz.de">Contact</a> | <a href="https://github.com/akrennmair/gnzkrz">Show me the code!</a> | <a href="{base}/_a/imprint.html">Impressum</a>
-<br>
-shortened {url_count} URLs that were accessed {access_count} times.
+<small>shortened {url_count} URLs that were accessed {access_count} times.</small>
 </div>
 $html_footer
 END
@@ -97,12 +139,38 @@ get '/_/preview/(.*)' => sub {
 	$sth->finish;
 	$dbh->disconnect;
 
+	my $html_menu = get_menu("preview");
+
 	t(<<END, base => base, base_url => base_url, id => $encoded_id, title => "krzz.de | Preview", url => cgi->escapeHTML($url), save => $len_diff );
 $html_header
-<div id=main>
-<h1>Preview</h1>
+$html_menu
+<div class="container">
+<h2>Preview</h2>
 <a href="{url}">{url}</a> has been shortened to <a href="{base_url}/{id}">{base_url}/{id}</a>, which is {save} bytes shorter.
 </div>
+$html_footer
+END
+};
+
+get '/_/faq' => sub {
+	my $html_menu = get_menu("faq");
+	my $faq_text = slurp("../_a/faq.html");
+	t(<<END, base => base, base_url => base_url, title => "krzz.de | FAQ");
+$html_header
+$html_menu
+$faq_text
+$html_footer
+END
+
+};
+
+get '/_/imprint' => sub {
+	my $html_menu = get_menu("imprint");
+	my $imprint_text = slurp("../_a/imprint.html");
+	t(<<END, base => base, base_url => base_url, title => "krzz.de | Impressum");
+$html_header
+$html_menu
+$imprint_text
 $html_footer
 END
 };
@@ -171,10 +239,17 @@ sub decode_id($) {
 
 sub error_unknown_url_id($) {
 	my $encoded_id = shift;
+
+	my $html_menu = get_menu("error_unknown_url");
+
 	return t(<<END, base => base, id => $encoded_id, title => "krzz.de | Error");
 $html_header
-<div id=main>
-<h1>Error: unknown URL {id}!</h1>
+$html_menu
+<div class="container">
+<div class="alert alert-error">
+<a class="close" data-dismiss="alert">×</a>
+<strong>Error:</strong> unknown URL {id}!
+</div>
 </div>
 $html_footer
 END
@@ -182,10 +257,17 @@ END
 
 sub error_invalid_url($) {
 	my $url = shift;
+
+	my $html_menu = get_menu("error_invalid_url");
+
 	return t(<<END, base => base, url => cgi->escapeHTML($url), title => "krzz.de | Error");
 $html_header
-<div id=main>
-<h1>Error: invalid URL {url}!</h1>
+$html_menu
+<div class="container">
+<div class="alert alert-error">
+<a class="close" data-dismiss="alert">×</a>
+<strong>Error:</strong> invalid URL {url}!
+</div>
 </div>
 $html_footer
 END
@@ -221,4 +303,12 @@ sub save_url($) {
 	my $encoded_id = encode_id($id);
 
 	return $encoded_id;
+}
+
+sub slurp($) {
+	my $fh;
+	open($fh, '<', shift) or return "";
+	my @lines = <$fh>;
+	close($fh);
+	return join("", @lines);
 }
